8b2367bccf7dc163b7bf0f7c1dfa24dc89bdfd9c
Обоев Рулон ибн Хаттаб <me@nomadrain.com>
2011-09-29 11:51:32 +0400

    Сделал красиво.

--- lib/chef/knife/clodo_server_start.rb
+++ lib/chef/knife/clodo_server_start.rb
@@ -13,10 +13,26 @@ class Chef
       :description => "The ID of the server",
       :proc => Proc.new { |f| Chef::Config[:knife][:server_id] = f.to_i }
 
+      option :all,
+      :long => "--all",
+      :description => "Start all the servers that not in \"is_running\" state."
+
       def run
         $stdout.sync = true
 
-        connection.start_server(Chef::Config[:knife][:server_id])
+        if config[:all] then
+
+          servers = connection.servers.all.select {|s| case s.state when 'is_running' then false when 'is_request' then false else true end }
+
+          servers.each do |server|
+            connection.start_server(server.id)
+          end
+
+        elsif Chef::Config[:knife][:server_id] then
+
+          connection.start_server(Chef::Config[:knife][:server_id])
+
+        end
 
       end
 
